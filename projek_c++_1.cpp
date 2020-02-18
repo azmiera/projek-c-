4f5389160013089d3d2a4cf0f3c2c58b378b0502
azmiera tarmizi <azmieratarmizi64@gmail.com>
2020-02-18 13:47:05 +0800

    okAY

---
+++ projek_c++_1.cpp
@@ -0,0 +1,943 @@
+//NAME:AZMIERA BINTI TARMIZI
+//DDWC2019/060548
+//PROGRAM DISPLAY A ALPHABET
+#include <iostream>
+using namespace std;
+int main()
+{
+	//DICLARE A VARIABLE
+	int i,j;
+	char opt,opt2;
+	
+	//START CHOOSE THE OUTPUT
+	do{
+	cout<<"INSERT THE ALPHABET: ";
+	cin>>opt; 
+	switch (opt)
+	{
+	case 'A':
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=4;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+   	break;
+	cout<<endl;
+	case 'B':
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=4;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+   for(i=1;i<=2;i++)
+	{
+		for(j=1;j<=18 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=4;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+   	break;
+	cout<<endl;
+	case 'C':
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=5;i++)
+	{
+	    for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+   	break;
+	cout<<endl;
+	case 'D':
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=18 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=6;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=18 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+   	break;
+	cout<<endl;
+	case 'E':
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+   for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=10 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+   for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=16;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+   for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=10 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+   	break;
+	cout<<endl;
+	case 'F':
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+   for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=10 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+   for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=16;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+   for(i=1;i<=6;i++)
+	{
+		for(j=1;j<=10 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+  	break;
+	cout<<endl;
+	case 'G':
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=10 ;j++)
+		    cout<<'*';
+	    for(j=1;j<=5;j++)
+		    cout<<' ';
+	    for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=24 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	
+    }
+  	break;
+	cout<<endl;
+	case 'H':
+	for(i=1;i<=5;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=5;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+  	break;
+	cout<<endl;
+	case 'I':
+	for(i=1;i<=15;i++)
+	{
+	for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+  	break;
+	cout<<endl;
+	case 'J':
+   for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=7;i++)
+	{
+	for(j=1;j<=13;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<=4;j++)
+		    cout<<' ';
+		for(j=1;j<=13 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+ 	break;
+	cout<<endl;
+	case 'K':
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=10;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=7;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=5;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<=15;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=5;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=7;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=10;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+ 	break;
+	cout<<endl;
+	case 'L':
+	for(i=1;i<=13;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	break;
+	cout<<endl;
+	case 'M':
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	    for(j=1;j<=26;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=20;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+		for(j=1;j<20;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	    for(j=1;j<=9;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	    for(j=1;j<=8;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=5;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	    for(j=1;j<=26;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+    break;
+	cout<<endl;
+	case 'N':
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=13;j++)
+	        cout<<'*';
+	    for(j=1;j<=23;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=26;j++)
+	        cout<<'*';
+	    for(j=1;j<=10;j++)
+		    cout<<' ';
+		for(j=1;j<=9;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	    for(j=1;j<=10;j++)
+		    cout<<' ';
+		for(j=1;j<7;j++)
+	        cout<<'*';
+	    for(j=1;j<=10;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	    for(j=1;j<=10;j++)
+		    cout<<' ';
+		for(j=1;j<=25;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	    for(j=1;j<=23;j++)
+		    cout<<' ';
+		for(j=1;j<13;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+    break;
+	cout<<endl;
+	case 'O':
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=5;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+    break;
+	cout<<endl;
+	case 'P':
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=5;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=2;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=10 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+	}
+    break;
+	cout<<endl;
+	case 'Q':
+	for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=4;j++)
+		    cout<<' ';
+	    for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=2;i++)
+	{
+		for(j=1;j<=22 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<=20;j++)
+		    cout<<' ';
+		for(j=1;j<=5 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+   break;
+	cout<<endl;
+	case 'R':
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=5;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=2;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+		
+	}
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=5;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=7;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=2;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=10;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+   break;
+	cout<<endl;
+	case 'S':
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+			cout<<endl;
+		
+	}
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+   for(i=1;i<=4;i++)
+   {
+   		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+   }
+   for(i=1;i<=3;i++)
+   {
+   	   for(j=1;j<=10;j++)
+		    cout<<' ';
+		for(j=1;j<=10 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+   }
+   for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+	
+	}
+  	break;
+	cout<<endl;
+	case 'T':
+    for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+	
+	}
+	for(i=1;i<=10;i++)
+	{
+    	for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+    break;
+	cout<<endl;
+	case 'U':
+	for(i=1;i<=10;i++)
+	{
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+	}
+	break;
+	cout<<endl;
+	case 'V':
+	for(i=1;i<=5;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	    for(j=1;j<=26;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=5;i++)
+	{
+	    for(j=1;j<=8;j++)
+	        cout<<' ';
+	    for(j=1;j<=10;j++)
+		    cout<<'*';
+		for(j=1;j<8;j++)
+	        cout<<' ';
+	    for(j=1;j<=10;j++)
+		    cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=18;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	    for(j=1;j<=8;j++)
+		    cout<<' ';
+	        cout<<endl;
+   }
+	break;
+	cout<<endl;
+	case 'W':
+	for(i=1;i<=5;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	    for(j=1;j<=26;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	    for(j=1;j<=9;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	    for(j=1;j<=8;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=20;j++)
+	        cout<<'*';
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+		for(j=1;j<20;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	    for(j=1;j<=26;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	break;
+	cout<<endl;
+	case 'X':
+	for(i=1;i<=5;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	    for(j=1;j<=9;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=5;i++)
+	{
+	for(j=1;j<=9;j++)
+		    cout<<' ';
+	    for(j=1;j<12;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=5;i++)
+	{
+	    for(j=1;j<=10;j++)
+	        cout<<'*';
+	    for(j=1;j<=9;j++)
+		    cout<<' ';
+		for(j=1;j<10;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+    break;
+	cout<<endl;
+	case 'Y':
+	for(i=1;i<=6;i++)
+	{
+	    for(j=1;j<=8;j++)
+	        cout<<'*';
+	    for(j=1;j<=9;j++)
+		    cout<<' ';
+		for(j=1;j<=8;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=10;i++)
+	{
+	for(j=1;j<=7;j++)
+		    cout<<' ';
+	    for(j=1;j<12;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+    break;
+	cout<<endl;
+	case 'Z': 
+	for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+    }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=9;j++)
+		    cout<<' ';
+	    for(j=1;j<12;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=6;j++)
+		    cout<<' ';
+	    for(j=1;j<12;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=3;j++)
+		    cout<<' ';
+	    for(j=1;j<12;j++)
+	        cout<<'*';
+	        cout<<endl;
+   }
+	for(i=1;i<=3;i++)
+	{
+	    for(j=1;j<=9;j++)
+		    cout<<'*';
+	    for(j=1;j<12;j++)
+	        cout<<' ';
+	        cout<<endl;
+   }
+for(i=1;i<=4;i++)
+	{
+		for(j=1;j<=20 ;j++)
+		    cout<<'*';
+		    cout<<endl;
+    }
+    break;
+    cout<<endl;
+    }
+    cout<<"DID YOU WANT TO CONTINUE? y/n:";
+	cin>>opt2;
+}while(opt2=='y'|| opt2=='Y');
+	return(0);
+	
+}
